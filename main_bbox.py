338b337909cc537500379ea25b3976769f524c42
Paul CHAINIEUX <paulchainieux@Pauls-MacBook-Air-2.local>
2025-04-14 07:39:23 +0100

    working bbox pipeline

--- main_bbox.py
+++ main_bbox.py
@@ -1,49 +1,81 @@
 import torch
 from torch.utils.data import DataLoader
+import os
 
-from src.bbox_utils import generate_pseudo_masks, generate_bboxs, generate_cams, evaluate, visualise_results, save_pseudo_masks
 from src.dataset import load_data_wrapper, PseudoMaskDataset
 from src.utils import load_device, clear_cuda_cache, parse_args
+from src.bbox_utils import generate_pseudo_mask_dataset_with_bbox, load_pseudo_mask
+from src.models import select_segmentation_model
+from src.fit import train_segmentation_model
+from src.metrics import evaluate_model, save_metrics_to_csv
 
 
 def main():
-    # Load configuration from .json config file (BBOX experiment)
-    config = parse_args(expriment_name="BBOX")
+
+    config = parse_args(expriment_name="CAM")
+    
+    # Set device and clear CUDA cache
+    DEVICE = load_device()
+    clear_cuda_cache()
+
+    ##################################################
+    # 1. Load dataset (train, val, and test loaders) #
+    ##################################################
 
     train_loader, val_loader, test_loader = load_data_wrapper(config=config)
-    
-    total_dice = 0.0
-    total_accuracy = 0.0
-    total_iou = 0.0
-    total_precision = 0.0
-    total_recall = 0.0
-    n_batches = len(train_loader)
-    batch_size = batch_size_test
-    print(n_batches)
-    output_dir = "saved_pseudo_masks"
-    os.makedirs(output_dir, exist_ok=True)
-    
-    for batch_idx, (image_batch, mask_batch, info_batch) in enumerate(train_loader, start=1):
-        print(f"Batch number: {batch_idx}")
-        cams = generate_cams(image_batch)
-        bboxs = generate_bboxs(cams, image_batch)
-        pseudo_masks = generate_pseudo_masks(bboxs, image_batch, variant="GrabCut")
-        dice, accuracy_score, iou, precision, recall = evaluate(pseudo_masks, mask_batch, batch_size)
-        print(f'Dice score is {dice} || Pixel score is {accuracy_score} || IOU score is {iou} || Precision score is {precision} || Recall score is {recall}')
-        total_dice += dice
-        total_accuracy += accuracy_score
-        total_iou += iou
-        total_precision += precision
-        total_recall += recall
-        visualise_results(image_batch[0], bboxs[0], mask_batch[0], pseudo_masks[0], variant="GrabCut")
-        save_pseudo_masks(pseudo_masks, batch_idx, output_dir)
-    
-    print(f'Average dice score per image is {total_dice / n_batches}')
-    print(f'Average accuracy score per image is {total_accuracy / n_batches}')
-    print(f'Average IOU score per image is {total_iou / n_batches}')
-    print(f'Average precision score per image is {total_precision / n_batches}')
-    print(f'Average recall score per image is {total_recall / n_batches}')
 
+    ############################################
+    # 2. Load or generate pseudo-masks dataset #
+    ############################################
+    
+    if config["dataset_output_dir"]:
+        generate_pseudo_mask_dataset_with_bbox(train_loader, config=config)
+    else:
+        pseudo_masks = load_pseudo_mask(config["dataset_dir"])  
+
+    ####################################
+    # 3. Initialise segmentation model #
+    ####################################
+    segmentation_model = select_segmentation_model(config["segmentation_model"])
+    if segmentation_model is None:
+            pass
+    segmentation_model.to(DEVICE)
+    
+
+    ###############################################################
+    # 4. Train segmentation model using the Bounding Boxes Dataset#
+    ###############################################################
+
+    train_dataset_pseudo = PseudoMaskDataset(original_dataset=train_loader.dataset,
+                                                 pseudo_masks=pseudo_masks
+                                                 )
+    train_loader_pseudo = DataLoader(train_dataset_pseudo, batch_size=config["train_batch_size"], shuffle=True, num_workers=0)
+    
+    # 4.2 Train or load baseline segmentation model
+    if config["train_segmentation"]:
+        print("\n----Training segmentation model...")
+        segmentation_model = train_segmentation_model(model=segmentation_model,
+                                                        train_loader=train_loader_pseudo,
+                                                        val_loader=val_loader,
+                                                        config=config,
+                                                        device=DEVICE
+                                                        )
+        torch.save(segmentation_model.state_dict(), config["weakseg_model_save_path"])
+        clear_cuda_cache()
+    else:
+        print("\n----Loading segmentation model...")
+        state_dict = torch.load(config["weakseg_model_save_path"], map_location=DEVICE, weights_only=True)
+        segmentation_model.load_state_dict(state_dict)
+        segmentation_model.to(DEVICE)
+        print("[Segmentation model loaded]")
+    
+    #####################################
+    # 5. Evaluate and visualize results #
+    #####################################
+    # 5.1 Evaluate segmentation model
+    metrics = evaluate_model(model=segmentation_model, test_loader=test_loader, threshold=0.5, device=DEVICE)
+    clear_cuda_cache()
+    save_metrics_to_csv(metrics, save_path=config["segmentation_metrics_save_path"])
 
 if __name__ == "__main__":
     main()
